c6f5e35b3f4cc12ef8b0654cccdb3921b2c5a558
warongkorn <warongkorn.y@motiftech.com>
2017-08-31 18:58:13 +0700

    Warongkorn

---
+++ Hello.java
@@ -0,0 +1,38 @@
+class Hello {
+    
+    public Hello() {
+        System.out.println("Hello! We are:");
+        this.Krerk();
+		
+		
+		
+		
+		this.Warongkorn();
+    }
+    
+    private void Krerk() {
+        System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+        System.out.println("Trainee - Krerk Piromsopa, Ph.D. --- ");
+    }
+    
+    public static void main(String [] argv) {
+        new Hello();
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 private void Warongkorn() {
+        System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+        System.out.println("Trainee - Warongkorn Yuwatthana");
+    }
+}
